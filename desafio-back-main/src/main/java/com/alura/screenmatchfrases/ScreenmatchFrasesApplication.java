c281cbd6d2e0a39f2783526c85c63c61346a336f
00Josh00 <josephchaparro08@gmail.com>
2024-05-21 19:38:50 -0500

    Creando la tabla frases

--- desafio-back-main/src/main/java/com/alura/screenmatchfrases/ScreenmatchFrasesApplication.java
+++ desafio-back-main/src/main/java/com/alura/screenmatchfrases/ScreenmatchFrasesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchFrasesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchFrasesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchFrasesApplication.class, args);
+    }
 
 }
