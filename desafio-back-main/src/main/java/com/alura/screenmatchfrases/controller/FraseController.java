c80feee6b4b414bd2b17ad26a5694d4f944cc7b4
00Josh00 <josephchaparro08@gmail.com>
2024-05-21 20:31:41 -0500

    conectando back y front

---
+++ desafio-back-main/src/main/java/com/alura/screenmatchfrases/controller/FraseController.java
@@ -0,0 +1,18 @@
+package com.alura.screenmatchfrases.controller;
+
+import com.alura.screenmatchfrases.dto.FraseDTO;
+import com.alura.screenmatchfrases.service.FraseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraseController {
+    @Autowired
+    private FraseService service;
+
+    @GetMapping("/series/frases")
+    public FraseDTO obtenerFrases(){
+        return service.obtenerFraseAleatoria();
+    }
+}
---
+++ desafio-back-main/src/main/java/com/alura/screenmatchfrases/dto/FraseDTO.java
@@ -0,0 +1,8 @@
+package com.alura.screenmatchfrases.dto;
+
+public record FraseDTO(
+        String titulo,
+        String frase,
+        String personaje,
+        String poster) {
+}
